a80b3ae63ebd26b04fcba3121aa6473920e52ad0
Mitul123 <sharmamitul72@gmail.com>
2022-01-20 23:13:58 +0530

    POST API call

--- main.go
+++ main.go
@@ -16,8 +16,20 @@ func main() {
 		})
 	})
 
+	r.POST("/create-short-url", func(c *gin.Context) {
+		handler.CreateShortUrl(c)
+	})
+
+	r.GET("/:shortUrl", func(c *gin.Context) {
+		handler.HandleShortUrlRedirect(c)
+	})
+
+	// Note store initialization happens here
+	store.InitializeStore()
+
 	err := r.Run(":9808")
 	if err != nil {
 		panic(fmt.Sprintf("Failed to start the web server - Error: %v", err))
 	}
+
 }
