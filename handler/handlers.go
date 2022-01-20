77b617a1827cadc913b533083a823b18e83e932a
Mitul123 <sharmamitul72@gmail.com>
2022-01-20 23:23:49 +0530

    import handler

--- handler/handlers.go
+++ handler/handlers.go
@@ -3,8 +3,8 @@ package handler
 import (
 	"net/http"
 
-	"go-shortner/shortner"
-	"go-shortner/store"
+	"github.com/Mitulsharma123/shortner"
+	"github.com/Mitulsharma123/store"
 
 	"github.com/gin-gonic/gin"
 )
