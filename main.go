3952438ccf0a4bc3727268e3016e119fa0aa77f1
Mitul123 <sharmamitul72@gmail.com>
2022-01-20 22:23:34 +0530

    adding main.go file

---
+++ main.go
@@ -0,0 +1,23 @@
+// Build a simple URL shortener service that will accept a URL as an argument over a REST API and
+//return a shortened URL as a result.
+
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{				
+			"message": "Hey Go URL Shortener !",	// should print the message 
+		})
+	})
+
+	err := r.Run(":9808")
+	if err != nil {
+		panic(fmt.Sprintf("Failed to start the web server - Error: %v", err))
+	}
+}
